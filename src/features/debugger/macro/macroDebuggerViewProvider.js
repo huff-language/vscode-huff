5601c66dce8960fa9e2da647124a72c3d6a3ab35
Cheethas <47148561+cheethas@users.noreply.github.com>
2022-09-02 23:22:39 +0100

    feat: Support debugging with call value

--- src/features/debugger/macro/macroDebuggerViewProvider.js
+++ src/features/debugger/macro/macroDebuggerViewProvider.js
@@ -1,143 +1,176 @@
 const vscode = require("vscode");
 
-const {getNonce, checkInputIsHex, checkCalldataIsHex} = require("../providerUtils");
-const {getMacros, getImports} = require("../../regexUtils");
-const {startMacroDebugger} = require("./macroDebugger");
-const { LANGUAGE_ID} = require("../../../settings");
-
+const {
+  getNonce,
+  checkInputIsHex,
+  checkCalldataIsHex,
+} = require("../providerUtils");
+const { getMacros, getImports } = require("../../regexUtils");
+const { startMacroDebugger } = require("./macroDebugger");
+const { LANGUAGE_ID } = require("../../../settings");
 
 /**Macro Debugger View Provider
- * 
+ *
  * Macro level debug webview
  */
-class MacroDebuggerViewProvider{
+class MacroDebuggerViewProvider {
+  static viewType = "huff.debugMacro";
 
-    static viewType = "huff.debugMacro";
+  constructor(extensionUri) {
+    this._extensionURI = extensionUri;
+    this._view = null;
+    this._macros = null;
+    this._currentFile = null;
+  }
 
-    constructor(extensionUri){
-        this._extensionURI = extensionUri;   
-        this._view = null;
-        this._macros = null;
-        this._currentFile = null; 
-    }
+  /**
+   *
+   * @param {*} webviewView
+   * @param {} context
+   * @param {*} _token
+   */
+  resolveWebviewView(webviewView, context, _token) {
+    // get the last active file
+    if (!this._currentFile && context && context.state)
+      this._currentFile = context.state.currentFile || null;
+    if (!this._macros && context && context.state)
+      this._macros = context.state.macros || null;
 
-    /**
-     * 
-     * @param {*} webviewView 
-     * @param {} context 
-     * @param {*} _token 
-     */
-    resolveWebviewView(
-        webviewView,
-        context,
-        _token
-    ){
-        // get the last active file
-        if (!this._currentFile && context && context.state) this._currentFile = context.state.currentFile || null;
-        if (!this._macros && context && context.state) this._macros = context.state.macros || null;
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [this._extensionURI],
+    };
 
-        webviewView.webview.options = {
-            enableScripts: true,
-            localResourceRoots: [
-                this._extensionURI
-            ]
+    // Get macros from the file
+    vscode.workspace.onDidSaveTextDocument((document) => {
+      // check the file type and only run on the current file
+      if (
+        document.languageId === LANGUAGE_ID &&
+        document.uri.scheme === "file" &&
+        document.uri.path === this._currentFile
+      ) {
+        this._macros = getMacros(
+          vscode.window.activeTextEditor?.document.getText()
+        );
+      }
+    });
+
+    webviewView.webview.html = this.getHtmlForWebView(webviewView.webview);
+
+    webviewView.webview.onDidReceiveMessage((data) => {
+      switch (data.type) {
+        case "loadMacros": {
+          const macros = getMacros(
+            vscode.window.activeTextEditor?.document.getText()
+          );
+          const activeFile = vscode.window.activeTextEditor?.document.uri.path;
+          this._currentFile = activeFile;
+          this._macros = macros;
+          this.addMacrosToOptions(macros, activeFile);
+          break;
         }
+        case "start-macro-debug": {
+          const {
+            macro,
+            argsArr,
+            stateChecked,
+            calldataChecked,
+            calldataValue,
+            storageChecked,
+            stateValues,
+            callValueChecked,
+            callValue,
+          } = data.values;
 
-        // Get macros from the file
-        vscode.workspace.onDidSaveTextDocument((document) => {
-            // check the file type and only run on the current file
-            if (
-                document.languageId === LANGUAGE_ID 
-                && document.uri.scheme === "file" 
-                && document.uri.path === this._currentFile 
-            ) {
-                this._macros = getMacros(vscode.window.activeTextEditor?.document.getText());
+          // If no macros are found then reload
+          if (!this._macros)
+            this._macros = getMacros(
+              vscode.window.activeTextEditor?.document.getText()
+            );
+          const macroBody = this._macros[macro];
+
+          // Prevent debugging and show an error message if the stack inputs are not hex
+          if (!checkInputIsHex(argsArr)) break;
+          if (calldataChecked && !checkCalldataIsHex(calldataValue)) break;
+
+          // get required file imports to flatten the file
+          const imports = getImports(
+            vscode.window.activeTextEditor?.document.getText()
+          );
+
+          startMacroDebugger(
+            vscode.workspace.getWorkspaceFolder(
+              vscode.window.activeTextEditor.document.uri
+            ).uri.path,
+            vscode.workspace.asRelativePath(
+              vscode.window.activeTextEditor.document.uri
+            ),
+            imports,
+            macroBody,
+            argsArr,
+            {
+              stateChecked,
+              calldataChecked,
+              calldataValue,
+              storageChecked,
+              stateValues,
+              calldataValue,
+              callValueChecked,
+              callValue,
             }
-        })
-
-        webviewView.webview.html = this.getHtmlForWebView(webviewView.webview);
-
-        webviewView.webview.onDidReceiveMessage(data => {
-            switch (data.type) {
-                case "loadMacros":{
-                    const macros = getMacros(vscode.window.activeTextEditor?.document.getText());
-                    const activeFile = vscode.window.activeTextEditor?.document.uri.path;
-                    this._currentFile = activeFile;
-                    this._macros = macros;
-                    this.addMacrosToOptions(macros, activeFile);
-                    break;
-                }
-                case "start-macro-debug": {
-                    const {
-                        macro, 
-                        argsArr, 
-                        stateChecked, 
-                        calldataChecked, 
-                        calldataValue,
-                        storageChecked,
-                        stateValues
-                    } = data.values;
-
-                    // If no macros are found then reload
-                    if (!this._macros) this._macros = getMacros(vscode.window.activeTextEditor?.document.getText());
-                    const macroBody = this._macros[macro];
-                    
-                    // Prevent debugging and show an error message if the stack inputs are not hex
-                    if (!checkInputIsHex(argsArr)) break;
-                    if (calldataChecked &&!checkCalldataIsHex(calldataValue)) break;
-
-                    // get required file imports to flatten the file
-                    const imports = getImports(vscode.window.activeTextEditor?.document.getText())
-                    
-                    startMacroDebugger(
-                        vscode.workspace.getWorkspaceFolder(vscode.window.activeTextEditor.document.uri).uri.path, 
-                        vscode.workspace.asRelativePath(vscode.window.activeTextEditor.document.uri), 
-                        imports, 
-                        macroBody, 
-                        argsArr, 
-                        {
-                            stateChecked,
-                            calldataChecked,
-                            calldataValue,
-                            storageChecked,
-                            stateValues
-                        });
-                }
-            }
-        });
-
-
-        this._view = webviewView;
-    }
-
-    addMacrosToOptions(macros, currentFile){
-        if (this._view){
-            this._view.show?.(true);
-            this._view.webview.postMessage({ type: "receiveMacros", data: macros, currentFile })
+          );
         }
+      }
+    });
+
+    this._view = webviewView;
+  }
+
+  addMacrosToOptions(macros, currentFile) {
+    if (this._view) {
+      this._view.show?.(true);
+      this._view.webview.postMessage({
+        type: "receiveMacros",
+        data: macros,
+        currentFile,
+      });
     }
+  }
 
-    updateSavedMacros(macros){
-        if (this._view){
-            this._view.show?.(true);
-            this._view.webview.postMessage({ type: "updateMacros", data: macros })
-        }
+  updateSavedMacros(macros) {
+    if (this._view) {
+      this._view.show?.(true);
+      this._view.webview.postMessage({ type: "updateMacros", data: macros });
     }
+  }
 
-    getHtmlForWebView(webview) {
-        // local path of main script to run in the webview
+  getHtmlForWebView(webview) {
+    // local path of main script to run in the webview
 
-        const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionURI, "webview", "macro", "macro.main.js"));
-        const helpersUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionURI, "webview", "helpers.js"));
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(
+        this._extensionURI,
+        "webview",
+        "macro",
+        "macro.main.js"
+      )
+    );
+    const helpersUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionURI, "webview", "helpers.js")
+    );
 
-        // Do the same for the stylesheet
-        const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionURI, "webview", "css", "vscode.css"));
-        const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionURI, "webview", "css", "main.css"));
+    // Do the same for the stylesheet
+    const styleVSCodeUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionURI, "webview", "css", "vscode.css")
+    );
+    const styleMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionURI, "webview", "css", "main.css")
+    );
 
-        // Use nonce to allow only a specific script to be run
-        const nonce = getNonce();
+    // Use nonce to allow only a specific script to be run
+    const nonce = getNonce();
 
-        return `<!DOCTYPE html>
+    return `<!DOCTYPE html>
                 <html>
                     <head>
                         <meta charset="UTF-8">
@@ -168,11 +201,15 @@ class MacroDebuggerViewProvider{
                             <label for="calldata-checkbox">With Calldata</label>
                             </br>
                             <input class="calldata-input" type="text" id="calldata-input" value="Enter Calldata">
-  
+
+                            <input class="call-value-checkbox" type="checkbox" id="call-value-checkbox" name="call-value-checkbox">
+                            <label for="call-value-checkbox">With Value</label>
+                            </br>
+                            <input class="call-value-input" type="text" id="call-value-input" value="Enter Value">
+
                             </br>
                             <input class="storage-checkbox" type="checkbox" id="storage-checkbox" name="storage-checkbox">
                             <label for="storage-checkbox">Storage Overrides</label>
-                            
                         </form>
 
                         
@@ -186,11 +223,9 @@ class MacroDebuggerViewProvider{
                         <script type="module" nonce="${nonce}" src="${helpersUri}"></script>
                      </body>
                 </html>`;
-        
-    }
+  }
 }
-
 
 module.exports = {
-    MacroDebuggerViewProvider
-}
+  MacroDebuggerViewProvider,
+};
